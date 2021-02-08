42d014140c1dcd2c126d0dc5fdbe4b1c6bcebb5b
Mårten Åsberg <marten.asberg@outlook.com>
2021-02-08 23:38:11 +0100

    Arithmetic Progression Game
    In addition to TicTacToe

--- executable/Main.hs
+++ executable/Main.hs
@@ -3,7 +3,7 @@
 
 module Main where
 
-import Data.List (intercalate)
+import Data.List (elemIndex, intercalate)
 import Data.Map (
     Map
   , elems
@@ -21,6 +21,7 @@ import MyLib (
   , strongPositionalGameGameOver
   , player
   )
+import System.IO (hFlush, stdout)
 import Prelude hiding (lookup)
 
 newtype TicTacToe = TicTacToe (Map (Integer, Integer) (Maybe Player))
@@ -73,5 +74,54 @@ instance PositionalGame TicTacToe (Integer, Integer) where
     , [(2, 0), (1, 1), (0, 2)]
     ]
 
+data ArithmeticProgressionGame = ArithmeticProgressionGame Int [Maybe Player]
+
+createArithmeticProgressionGame :: Int -> Int -> Maybe ArithmeticProgressionGame
+createArithmeticProgressionGame n k = if k < n
+  then Just $ ArithmeticProgressionGame k (replicate n Nothing)
+  else Nothing
+
+instance Show ArithmeticProgressionGame where
+  show (ArithmeticProgressionGame _ ps) = (\(is, ps) -> intercalate "," is ++ "\n" ++ intercalate "," ps) $
+        unzip $ zipWith (\i p -> (pad $ show i, pad $ showP p)) [1..] ps
+    where
+      showP Nothing        = "_"
+      showP (Just Player1) = "O"
+      showP (Just Player2) = "X"
+      pad x = replicate (3 - length x) ' ' ++ x
+
+instance StrongPositionalGame ArithmeticProgressionGame Int where
+  position (ArithmeticProgressionGame _ l) i = if i <= length l then Just $ l !! (i - 1) else Nothing
+  positions (ArithmeticProgressionGame _ l) = l
+  setPosition (ArithmeticProgressionGame k l) i p = if i <= length l
+    then Just $ ArithmeticProgressionGame k (take (i - 1) l ++ Just p : drop i l)
+    else Nothing
+
+instance PositionalGame ArithmeticProgressionGame Int where
+  makeMove = strongPositionalGameMakeMove
+  gameOver a@(ArithmeticProgressionGame k l) = let n = length l
+    in strongPositionalGameGameOver (filter (all (<= n)) $ concat [[take k [i,i+j..] | j <- [1..n-i]] | i <- [1..n]]) a
+
 main :: IO ()
-main = player emptyTicTacToe
+main = do
+  putStrLn "1: TicTacToe"
+  putStrLn "2: Arithmetic Progression Game"
+  putStr "What do you want to play? "
+  hFlush stdout
+  choice <- read <$> getLine
+  case choice of
+    1 -> player emptyTicTacToe
+    2 -> playAPG
+    _ -> putStrLn "Invalid choice!"
+
+playAPG :: IO ()
+playAPG = do
+  putStr "n: "
+  hFlush stdout
+  n <- read <$> getLine
+  putStr "k: "
+  hFlush stdout
+  k <- read <$> getLine
+  case createArithmeticProgressionGame n k of
+    Just a -> player a
+    Nothing -> putStrLn "Not valid input (n < k)"
