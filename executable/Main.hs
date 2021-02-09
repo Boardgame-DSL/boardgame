9313fc97bf8fd2b5c1027e419ea12619ea132ba2
Mårten Åsberg <marten.asberg@outlook.com>
2021-02-09 15:44:41 +0100

    Shannon Switching Game
    Co-authored-by: jenniferkrogh <jennifer.krogh8@gmail.com>
    Co-authored-by: zinfour <zinfour17@gmail.com>

--- executable/Main.hs
+++ executable/Main.hs
@@ -3,7 +3,13 @@
 
 module Main where
 
-import Data.List (elemIndex, intercalate)
+import Data.Graph (Graph, buildG, path)
+import Data.List (
+    elemIndex
+  , find
+  , findIndex
+  , intercalate
+  )
 import Data.Map (
     Map
   , elems
@@ -13,6 +19,7 @@ import Data.Map (
   , member
   , (!)
   )
+import Data.Maybe (fromJust, isJust)
 import MyLib (
     Player(..)
   , PositionalGame(..)
@@ -23,6 +30,10 @@ import MyLib (
   )
 import System.IO (hFlush, stdout)
 import Prelude hiding (lookup)
+
+-------------------------------------------------------------------------------
+-- * TicTacToe
+-------------------------------------------------------------------------------
 
 newtype TicTacToe = TicTacToe (Map (Integer, Integer) (Maybe Player))
 
@@ -74,6 +85,10 @@ instance PositionalGame TicTacToe (Integer, Integer) where
     , [(2, 0), (1, 1), (0, 2)]
     ]
 
+-------------------------------------------------------------------------------
+-- * Arithmetic Progression Game
+-------------------------------------------------------------------------------
+
 data ArithmeticProgressionGame = ArithmeticProgressionGame Int [Maybe Player]
 
 createArithmeticProgressionGame :: Int -> Int -> Maybe ArithmeticProgressionGame
@@ -102,16 +117,73 @@ instance PositionalGame ArithmeticProgressionGame Int where
   gameOver a@(ArithmeticProgressionGame k l) = let n = length l
     in strongPositionalGameGameOver (filter (all (<= n)) $ concat [[take k [i,i+j..] | j <- [1..n-i]] | i <- [1..n]]) a
 
+-------------------------------------------------------------------------------
+-- * Shannon Switching Game
+-------------------------------------------------------------------------------
+
+newtype ShannonSwitchingGame = ShannonSwitchingGame (Int, [((Int, Int), Maybe Player)])
+
+-- | Creates a list of all edges, input n gives n*n graph
+gridEdges :: Int -> [((Int, Int), Maybe Player)]
+gridEdges n =
+  concat [[((j+i*n,(j+1)+i*n), Nothing), ((j+i*n,j+(i+1)*n), Nothing)] | i <- [0..n-2], j <- [0..n-2]] ++
+  [(((n-1)+i*n, (n-1)+(i+1)*n), Nothing) | i <- [0..n-2]] ++
+  [((i+(n-1)*n, (i+1)+(n-1)*n), Nothing) | i <- [0..n-2]]
+
+createShannonSwitchingGame :: Int -> ShannonSwitchingGame
+createShannonSwitchingGame n = ShannonSwitchingGame (n, gridEdges n)
+
+-- o───o---o
+-- ║   │   :
+-- o═══o═══o
+-- ║   │   :
+-- o───o───o
+instance Show ShannonSwitchingGame where
+  show a@(ShannonSwitchingGame (n, l)) = intercalate "\n" ([concat ["o" ++ showH (fromJust $ position a (i+j*n, (i+1)+j*n)) | i <- [0 .. n - 2]]
+    ++ "o\n" ++ concat [showV (fromJust $ position a (i+j*n, i+(j+1)*n)) ++ "   " | i <- [0 .. n - 2]] ++ showV (fromJust $ position a ((n-1)+j*n, (n-1)+(j+1)*n) ) |
+    j <- [0 .. n - 2]]
+    ++ [concat ["o" ++ showH (fromJust $ position a (i+(n-1)*n, (i+1)+(n-1)*n)) | i <- [0 .. n - 2]] ++ "o"])
+    where
+      showH (Just Player1) = "───"
+      showH (Just Player2) = "═══"
+      showH Nothing        = "---"
+      showV (Just Player1) = "│"
+      showV (Just Player2) = "║"
+      showV Nothing        = ":"
+
+instance StrongPositionalGame ShannonSwitchingGame (Int, Int) where
+  position (ShannonSwitchingGame (_, l)) c = snd <$> find ((== c) . fst) l
+  positions (ShannonSwitchingGame (_, l)) = map snd l
+  setPosition (ShannonSwitchingGame (n, l)) c p = case findIndex ((== c) . fst) l of
+    Just i -> Just $ ShannonSwitchingGame (n, take i l ++ (c, Just p) : drop (i + 1) l)
+    Nothing -> Nothing
+
+instance PositionalGame ShannonSwitchingGame (Int, Int) where
+  makeMove = strongPositionalGameMakeMove
+  gameOver (ShannonSwitchingGame (n, l))
+    | path g 0 (n * n - 1) = Just (Just Player1)
+    | path g (n - 1) (n * n - n) = Just (Just Player2)
+    | all (isJust . snd) l = Just Nothing
+    | otherwise = Nothing
+    where
+      g = buildG (0, n * n - 1) (map fst $ filter ((== Just Player1) . snd) l)
+
+-------------------------------------------------------------------------------
+-- * CLI interactions
+-------------------------------------------------------------------------------
+
 main :: IO ()
 main = do
   putStrLn "1: TicTacToe"
   putStrLn "2: Arithmetic Progression Game"
+  putStrLn "3: Shannon Switching Game"
   putStr "What do you want to play? "
   hFlush stdout
   choice <- read <$> getLine
   case choice of
     1 -> player emptyTicTacToe
     2 -> playAPG
+    3 -> player $ createShannonSwitchingGame 5
     _ -> putStrLn "Invalid choice!"
 
 playAPG :: IO ()
