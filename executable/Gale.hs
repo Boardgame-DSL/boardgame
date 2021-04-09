c7c3b8de3a4d144a95d238afd6f2a22d2b05035f
Mårten Åsberg <marten.asberg@outlook.com>
2021-04-09 12:57:48 +0200

    Fully working implementation of Gale
    A Shannon Switching Game with a different UI.

--- executable/Gale.hs
+++ executable/Gale.hs
@@ -5,43 +5,31 @@ module Gale where
 
 import Data.Graph as Graph (Graph, buildG, path)
 import Data.List (intercalate)
-import Prelude hiding (lookup)
-
-import Data.Map (
-    Map
-  , assocs
-  , elems
-  , fromList
-  , insert
-  , lookup
-  , member
-  , (!)
-  )
+import Data.Maybe (fromJust, mapMaybe)
 
 import Boardgame (
     Player(..)
   , Position(..)
-  , Outcome(..)
   , PositionalGame(..)
-  , isOccupied
   )
 
-
+import ShannonSwitchingGame
 
 -------------------------------------------------------------------------------
 -- * Gale
 -------------------------------------------------------------------------------
 
-newtype Gale = Gale (Map (Integer, Integer) Position)
+newtype Gale = Gale ShannonSwitchingGameCG
 
--- Creates an empty Gale playfield. Even "rows" have 4 "columns" and odd ones
--- have 3.
 emptyGale :: Gale
-emptyGale = Gale $
-  fromList $
-    zip
-      ([0..8] >>= (\y -> [(x, y) | x <- [0..(4 - y `rem` 2)]]))
-      (repeat Empty)
+emptyGale = Gale $ createEmptyShannonSwitchingGameCG connections (-1) (-2)
+  where
+    connections = mapMaybe galeCoordinatesToId [(x, y) | x <- [0..8], y <- [0..8]]
+
+galeCoordinatesToId :: (Int, Int) -> Maybe (Int, Int)
+galeCoordinatesToId (x, y) | even x && even y = Just (if x == 0 then -1 else x + y * 4 - 2, if x == 8 then -2 else x + y * 4)
+                           | odd x && odd y   = Just (x + (y - 1) * 4 - 1, x + (y - 1) * 4 + 7)
+                           | otherwise        = Nothing
 
 --    0 1 2 3 4 5 6 7 8
 --    ╔═══╦═══╦═══╦═══╗
@@ -56,7 +44,7 @@ emptyGale = Gale $
 -- 8└   ┴   ┴   ┴   ┴   ┘
 --    ╚═══╩═══╩═══╩═══╝
 instance Show Gale where
-  show (Gale b) = intercalate "\n" [
+  show g = intercalate "\n" [
         "   0 1 2 3 4 5 6 7 8  "
       , "   \ESC[31m╔═══╦═══╦═══╦═══╗\ESC[0m  "
       , "0\ESC[34m┌" ++ intercalate "\ESC[34m┬" (row 0) ++ "\ESC[34m┐\ESC[0m"
@@ -72,7 +60,7 @@ instance Show Gale where
       ]
     where
       -- "Shows" the elements of the given row
-      row y = map (\x -> showP (b ! (x, y)) y) [0..(4 - y `rem` 2)]
+      row y = mapMaybe (\x -> flip showP y <$> getPosition g (x, y)) [0..8]
       showP (Occupied Player1) y
         | even y      = "\ESC[34m───"
         | otherwise   = " \ESC[34m│ "
@@ -81,26 +69,8 @@ instance Show Gale where
         | otherwise   = "\ESC[31m═══"
       showP Empty _   = "   "
 
-instance PositionalGame Gale (Integer, Integer) where
-  getPosition (Gale b) (x, y) = if x `rem` 2 == y `rem` 2 then lookup c b else Nothing
-    where c = (x `div` 2, y)
-  positions (Gale b) = elems b
-  setPosition (Gale b) (x, y) p = if x `rem` 2 == y `rem` 2 && member c b then Just $ Gale $ insert c p b else Nothing
-    where c = (x `div` 2, y)
-  gameOver (Gale b)
-    | path player1Graph (-1) (-2) = Just $ Win Player1
-    | path player2Graph (-1) (-2) = Just $ Win Player2
-    | all isOccupied (elems b) = Just Draw
-    | otherwise            = Nothing
-    where
-      playerGraph from to p = buildG (-2, 19) $
-        filter ((Occupied p ==) . snd) (assocs b) >>=
-          ((\(x, y) -> [(x, y), (y, x)]) . (\((x, y), _) -> (fromInteger $ from x y, fromInteger $ to x y)))
-      player1Graph = playerGraph
-        (\x y -> if x == 0 then -1 else (y `div` 2) + 5 * (x + (y `rem` 2) - 1))
-        (\x y -> if x == 4 then -2 else (y `div` 2) + (y `rem` 2) + 5 * x)
-        Player1
-      player2Graph = playerGraph
-        (\x y -> if y == 0 then -1 else x + 5 * ((y `div` 2) + (y `rem` 2) - 1))
-        (\x y -> if y == 8 then -2 else x + (y `rem` 2) + 5 * (y `div` 2))
-        Player2
+instance PositionalGame Gale (Int, Int) where
+  getPosition (Gale b) coords = galeCoordinatesToId coords >>= getPosition b
+  positions (Gale b) = positions b
+  setPosition (Gale b) coords p = Gale <$> (galeCoordinatesToId coords >>= flip (setPosition b) p)
+  gameOver (Gale b) = gameOver b
